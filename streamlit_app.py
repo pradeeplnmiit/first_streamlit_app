57e48653f5f73a20ecf8907dae3f644d951996cb
Pradeep Singh <pradeep.lnmiit@gmail.com>
2022-08-24 16:49:36 +1000

    exception handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,17 +23,21 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = streamlit.text_input('what fruit would you like information about?','kiwi')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-fruit_choice = streamlit.text_input('what fruit would you like information about?','kiwi')
-streamlit.write('The user entered',fruit_choice)
+    #take the json version of the response and normalize it
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    #output it on the screen as a table
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
-#take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-#output it on the screen as a table
-streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
 
