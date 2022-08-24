e11886aaa0c54b4020b19df054223f5604e74983
Pradeep Singh <pradeep.lnmiit@gmail.com>
2022-08-24 14:34:38 +1000

    Customised user input

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,10 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruit_choice = streamlit.text_input('what fruit would you like information about?','kiwi')
+streamlit.write('The user entered',fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 #take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
